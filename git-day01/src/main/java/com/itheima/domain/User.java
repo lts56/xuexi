3089328bcfaa42688cfa477c4e0f37eb0f9c8b8c
lts <705416560@qq.com>
2018-10-19 20:57:09 +0800

    新建user.java提交到本地仓库

---
+++ git-day01/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,35 @@
+package com.itheima.domain;
+
+/**
+ * @Author: lts
+ * @Date: 2018/10/19 20:44
+ */
+public class User {
+
+    private String name;//年龄
+    private int math;//数学成绩
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", math=" + math +
+                '}';
+    }
+}
